98e2e94cf87bbd7c6c421e01adc29d1834f20892
Tim Colson <timcolson@gmail.com>
2026-01-21 10:58:11 -0700

    Fix navigation issue by adding site base config

--- workshop-docs/astro.config.mjs
+++ workshop-docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://timcolson.github.io',
+	base: '/workshop-1001',
 	integrations: [
 		starlight({
 			title: 'HTMX & Datastar Workshop',
